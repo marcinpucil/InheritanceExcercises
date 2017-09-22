184a8f3f6b3b97a195bbd1d74a790660f2c5f5ea
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-22 23:44:46 +0200

    Excercise1 solved v.2.0

--- src/Excercise1/LabeledPoint.java
+++ src/Excercise1/LabeledPoint.java
@@ -34,8 +34,6 @@ public class LabeledPoint extends Point {
         return Objects.hash(super.hashCode(), getLabel());
     }
 
-
-
     @Override
     protected LabeledPoint clone() throws CloneNotSupportedException {
         return (LabeledPoint) super.clone();
