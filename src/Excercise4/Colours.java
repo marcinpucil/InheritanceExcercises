f71ad5438b954df64b072970710601c141f4158b
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-27 00:44:31 +0200

    Excercise4 solved v.2.0

--- src/Excercise4/Colours.java
+++ src/Excercise4/Colours.java
@@ -1,39 +1,17 @@
 package Excercise4;
 
 public enum Colours {
-    RED {
-    public Colours getRed() {
+    RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+
+    public static Colours getRed() {
         return Colours.RED;
     }
-},
-    BLUE {
-        public Colours getBlue() {
-            return Colours.BLUE;
-        }
-    },
-    GREEN {
-        public Colours getGreen() {
-            return Colours.GREEN;
-        }
-    },
-    CYAN {
-        public Colours getCyan() {
-            return Colours.CYAN;
-        }
-    },
-    MAGENTA {
-        public Colours getMagenta() {
-            return Colours.MAGENTA;
-        }
-    },
-    YELLOW {
-        public Colours getYellow() {
-            return Colours.YELLOW;
-        }
-    },
-    WHITE {
-        public Colours getWhite() {
-            return Colours.WHITE;
-        }
+
+    public static Colours getBlue() {
+        return Colours.BLUE;
+    }
+
+    public static Colours getGreen() {
+        return Colours.GREEN;
     }
 }
--- src/Excercise4/Main.java
+++ src/Excercise4/Main.java
@@ -1,21 +1,13 @@
-/*Excercise3
-Załóżmy, że w podrozdziale 4.2.2, „Metoda equals”, metoda Item.equals korzysta
-z testu instanceof. Zaimplementuj DiscountedItem.equals w taki sposób,
-by porównywała tylko klasę nadrzędną, jeśli otherObject jest klasy Item, ale również
-brała pod uwagę zniżkę, jeśli jest ona klasy DiscountedItem. Pokaż, że ta metoda
-zachowuje symetrię, ale nie jest przechodnia — czyli znajdź takie instancje obu klas,
-by prawdziwe było x.equals(y) i y.equals(z), ale nie x.equals(z).
-*/
+package Excercise4;/*Excercise3
+Zdefiniuj typ wyliczeniowy dla ośmiu kombinacji kolorów podstawowych: BLACK,
+RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE z metodami: getRed, getGreen i getBlue.
 package Excercise4;
-
-import Excercise3.DiscountedItem;
-import Excercise3.Item;
-
+*/
 public class Main {
 
     public static void main(String[] args) {
-        Colours red = Colours.RED;
-
-
+        Colours colours;
+        colours = Colours.getBlue();
+        System.out.println(colours);
     }
 }
