e875e70528a3953af54389b0416c6a673108a29e
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-27 23:47:45 +0200

    Excercise4 second part v.2.2

--- src/Excercise4/Main.java
+++ src/Excercise4/Main.java
@@ -1,13 +1,33 @@
 package Excercise4;/*Excercise3
 Zdefiniuj typ wyliczeniowy dla ośmiu kombinacji kolorów podstawowych: BLACK,
 RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE z metodami: getRed, getGreen i getBlue.
-package Excercise4;
+cd.
+Klasa Class ma sześć metod zwracających ciąg znaków opisujący typ reprezentowany
+przez obiekt Class. Czym się różnią, gdy użyte są dla tablicy, zwykłego typu, klasy
+wewnętrznej i typu prostego?
 */
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] tableForClassTest = {1, 2, 3};
+
         Colours colours;
         colours = Colours.getBlue();
         System.out.println(colours);
+        //second part
+        secondPart(tableForClassTest);
     }
+
+    private static void secondPart(Object objectForTest) {
+        Class<?> aClass = objectForTest.getClass();
+        System.out.println(aClass.getCanonicalName());
+        System.out.println(aClass.getName());
+        System.out.println(aClass.getSimpleName());
+        System.out.println(aClass.getTypeName());
+        System.out.println(aClass.toGenericString());
+        System.out.println(aClass.toString());
+    }
+
+
 }
