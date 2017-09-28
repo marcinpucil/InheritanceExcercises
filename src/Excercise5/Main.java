c1b75fe3fef71d6e81a44e143b178bded64e1342
marcinpucil <marcin.pucilowskiii@gmail.com>
2017-09-28 23:24:34 +0200

    Excercise5 solved v.1.0

--- src/Excercise5/Main.java
+++ src/Excercise5/Main.java
@@ -9,17 +9,19 @@ import java.lang.reflect.Field;
 
 public class Main {
     private java.lang.String string = "cos";
+    private Integer integer = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         Main main = new Main();
         System.out.println(uniwersalToStringMethod(main));
     }
 
-    private static java.lang.String uniwersalToStringMethod(Object o) {
+    private static java.lang.String uniwersalToStringMethod(Object o) throws IllegalAccessException {
         StringBuilder stringBuilder = new StringBuilder();
         Class<?> aClass = o.getClass();
         for (Field field : aClass.getDeclaredFields()) {
-            stringBuilder.append(field.getName());
+            field.setAccessible(true);
+            stringBuilder.append(field.getName()).append(" ").append(field.get(o)).append(" ");
         }
         return stringBuilder.toString();
     }
